92f1da17da80234e0889b998bb25da4c8202c57f
myothra7777 <turtwigandmew@yahoo.com>
2020-05-13 15:57:28 -0700

    Setting Up Online Hosting

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dylan/Documents/CINS465/CINS465-S20-Dylan-Davidson-master/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
